8cb49bf11acea3951bce840c05fc77f4b418cae9
dantmnf <dantmnf@users.noreply.github.com>
2023-07-21 17:40:54 +0800

    wait for WindowManagerService#mSystemBooted

--- magisk/module/service.sh
+++ magisk/module/service.sh
@@ -1,13 +1,10 @@
-
-wait_service() {
-  while service check "$1" | grep -q 'not found'
-  do
-    sleep 1
-  done
-}
-
-wait_service display
-wait_service settings
+# query WindowManagerService for mSystemBooted=true
+count=0
+while let "count++ < 100"
+do
+  dumpsys window | grep -q mSystemBooted=true && break
+  sleep 1
+done
 
 rate="$(settings get global user_preferred_refresh_rate)"
 height="$(settings get global user_preferred_resolution_height)"
